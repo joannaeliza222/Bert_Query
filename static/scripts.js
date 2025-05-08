f682341bede5f7c2ad3e04a6c6808229b44ff56c
joannaeliza222 <ejoanna222@gmail.com>
2025-05-08 11:20:15 +0530

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -4,6 +4,8 @@ function clearFields() {
     document.querySelector('input[name="memo_id"]').value = '';
     document.querySelector('select[name="state_name"]').selectedIndex = 0;
     document.getElementById('related-questions-box').innerHTML = ''; // Clear related questions
+    document.getElementById('related_count').innerHTML = ''; 
+    document.getElementById('download_result').innerHTML = '';
 }
 
 
